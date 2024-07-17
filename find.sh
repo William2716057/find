5ac29544b45dc0d3719226c7dec0b6e2761d7405
William <willspri@gmail.com>
2024-07-17 13:21:58 +1200

    cleaned

--- find.sh
+++ find.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Execute the PowerShell script
-
-#powershell ./find.ps1
-#powershell Get-ChildItem -Path C:\ -Recurse -Filter "sample.txt"
 powershell.exe -Command "Get-ChildItem -Path C:\ -Recurse -Filter 'sample.txt'"
